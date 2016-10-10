00f01a03539798e5af42aa87bb8b4331d48af36b
newman26 <bnewman@live.fr>
2016-10-11 01:26:25 +0200

    commit petites evolutions

--- 07_TP_JSF_EJB_JPA/src/fr/adaming/managedBeans/AgentManagedBean.java
+++ 07_TP_JSF_EJB_JPA/src/fr/adaming/managedBeans/AgentManagedBean.java
@@ -9,7 +9,7 @@ import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
